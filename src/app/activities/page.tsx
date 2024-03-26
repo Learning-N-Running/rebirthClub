7506be0ad7ab8089a924243d8bb6b449ceff7ea2
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2024-03-26 20:16:52 +0900

    Feat: activities page

---
+++ src/app/activities/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+import Activity from "@/components/Activity";
+import InstructionTitle from "@/components/InstructionTitle";
+import Timeline from "@/components/TimeLine";
+import { styled } from "styled-components";
+import activities from "../../lib/activities.json";
+
+export default function Activities() {
+  return (
+    <Container>
+      <ActivitiesWrapper>
+        <InstructionTitle
+          isIcon={true}
+          title={"활동별 NFT"}
+          detail={"2개 이상 발급 시,  환생클럽 인증서 NFT 발급이 가능합니다."}
+        />
+        <div style={{ display: "flex", marginTop: "60px" }}>
+          <Timeline events={activities} />
+          <div>
+            {activities.map((activity, index) => (
+              <Activity
+                imgSrc={activity.imgSrc}
+                name={activity.name}
+                date={activity.date}
+                index={activity.index}
+                key={index}
+              />
+            ))}
+          </div>
+        </div>
+      </ActivitiesWrapper>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding-bottom: 50px;
+`;
+
+const ActivitiesWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
